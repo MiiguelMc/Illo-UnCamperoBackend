d6e97a2eeb1ce2761af329be744950d2ec184b5d
MiiguelMc <mmacpin1111@g.educaand.es>
2026-01-22 18:51:48 +0100

    fix : Guarda nuevo Pedido

--- Backend/src/main/java/com/illouncampero/Backend/config/SecurityConfig.java
+++ Backend/src/main/java/com/illouncampero/Backend/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
+    
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
