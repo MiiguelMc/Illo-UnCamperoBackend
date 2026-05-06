e10c46a2145029e80cdfcb2268e7fd11f960f474
Miguel <miguelmaciasdelpino@gmail.com>
2026-05-06 22:34:20 +0200

    fix: valores por defecto en vars Cloudinary para evitar crash al arrancar + apellidos en Usuario

--- Backend/src/main/java/com/illouncampero/Backend/model/Usuario.java
+++ Backend/src/main/java/com/illouncampero/Backend/model/Usuario.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 public class Usuario {
     private String uid;
     private String nombre;
+    private String apellidos;
     private String email;
     private String telefono;
     private String direccion;
